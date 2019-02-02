62be1b56def39084f744d5851ca39d06c490fe17
Luke Myers <19lmyers@gmail.com>
2019-02-02 15:54:50 -0800

    Revert "started work on bus between rioduino and robotrio"
    This utterly failed to work and we need to strart over.
    
    This reverts commit 820c376f9d3bc1d3db6a73b98cd30c098863c978.

--- 2019-Coprocessor.ino
+++ 2019-Coprocessor.ino
@@ -1,53 +1,25 @@
-#include <Wire.h>
-
 #include "AbsoluteEncoder.h"
 #include "VisionSystem.h"
 
-const int RIODUINO_DEVICE_ADDRESS = 8;
-
-enum Command {
-  COMMAND_1,
-  COMMAND_2
-};
-
-int num;
-
-//AbsoluteEncoder encoders(3, 6, 7);
-//VisionSystem visionSystem;
+AbsoluteEncoder encoders(3, 6, 7);
+VisionSystem visionSystem;
 
 void setup() {
-  Wire.begin(RIODUINO_DEVICE_ADDRESS);
-  Wire.onRequest(onRequestData);
-  Wire.onReceive(onReceiveCommand);
-
-  num = 16384;
+  Serial.begin(9600);
 }
 
 void loop() {
-  //encoders.readAll();
-  //visionSystem.readBlocks();
+  encoders.readAll();
+  visionSystem.readBlocks();
+
+  Serial.print("encoder 1: ");
+  Serial.println(encoders.getDegrees(0));
+
+  Serial.print("encoder 2: ");
+  Serial.println(encoders.getDegrees(1));
+
+  Serial.print("encoder 3: ");
+  Serial.println(encoders.getDegrees(2));
 
   delay(20);
-
-}
-
-
-void onRequestData() {
-   byte buff[4];
-
-   buff[0] = num >> 24;
-   buff[1] = num >> 16;
-   buff[2] = num >> 8;
-   buff[3] = num;
-  
-   Wire.write(buff, 4);
-}
-
-
-void onReceiveCommand(int howMany) {
-  if (Wire.available() > 0) {
-    int c = Wire.read(); // receive int as a enum
-  
-    num = c;
-  }
 }
