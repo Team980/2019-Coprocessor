820c376f9d3bc1d3db6a73b98cd30c098863c978
EtomicBomb <ethan@ethan.ws>
2019-02-02 15:09:35 -0800

    started work on bus between rioduino and robotrio

--- 2019-Coprocessor.ino
+++ 2019-Coprocessor.ino
@@ -1,25 +1,53 @@
+#include <Wire.h>
+
 #include "AbsoluteEncoder.h"
 #include "VisionSystem.h"
 
-AbsoluteEncoder encoders(3, 6, 7);
-VisionSystem visionSystem;
+const int RIODUINO_DEVICE_ADDRESS = 8;
+
+enum Command {
+  COMMAND_1,
+  COMMAND_2
+};
+
+int num;
+
+//AbsoluteEncoder encoders(3, 6, 7);
+//VisionSystem visionSystem;
 
 void setup() {
-  Serial.begin(9600);
+  Wire.begin(RIODUINO_DEVICE_ADDRESS);
+  Wire.onRequest(onRequestData);
+  Wire.onReceive(onReceiveCommand);
+
+  num = 16384;
 }
 
 void loop() {
-  encoders.readAll();
-  visionSystem.readBlocks();
-
-  Serial.print("encoder 1: ");
-  Serial.println(encoders.getDegrees(0));
-
-  Serial.print("encoder 2: ");
-  Serial.println(encoders.getDegrees(1));
-
-  Serial.print("encoder 3: ");
-  Serial.println(encoders.getDegrees(2));
+  //encoders.readAll();
+  //visionSystem.readBlocks();
 
   delay(20);
+
+}
+
+
+void onRequestData() {
+   byte buff[4];
+
+   buff[0] = num >> 24;
+   buff[1] = num >> 16;
+   buff[2] = num >> 8;
+   buff[3] = num;
+  
+   Wire.write(buff, 4);
+}
+
+
+void onReceiveCommand(int howMany) {
+  if (Wire.available() > 0) {
+    int c = Wire.read(); // receive int as a enum
+  
+    num = c;
+  }
 }
