e0d5be474debf852eb0a2ac6da3acf8ff79b43e8
Luke Myers <19lmyers@gmail.com>
2019-02-16 14:58:27 -0800

    Working daisy chain system for absolute encoders!!

--- 2019-Coprocessor.ino
+++ 2019-Coprocessor.ino
@@ -1,9 +1,9 @@
 #include <Wire.h>
 
-//#include "AbsoluteEncoder.h"
+#include "AbsoluteEncoder.h"
 #include "VisionSystem.h"
 
-//AbsoluteEncoder encoders(3, 6, 7);
+AbsoluteEncoder encoders(3, 6, 7); //data, clock, chip select
 VisionSystem visionSystem;
 
 void setup() {
@@ -15,7 +15,7 @@ void setup() {
 }
 
 void loop() {
-  //encoders.readAll();
+  encoders.readAll();
   visionSystem.readBlocks();
 
   delay(20);
@@ -31,6 +31,11 @@ void onRequestData() {
   int targetWidth = visionSystem.getTargetWidth();
   buff[2] = targetWidth >> 8;
   buff[3] = targetWidth;
+
+  //uint32_t degree0 = encoders.getDegrees(0);
+  //uint32_t degree1 = encoders.getDegrees(1);
+  //uint32_t degree2 = encoders.getDegrees(2);
+  //TODO implement on roboRIO
 
   Wire.write(buff, 4);
 }
